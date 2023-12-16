1e0013acb0fd47e683f5aaba5ee50d5e7800cb97
Logan <110278043+logan-m-price@users.noreply.github.com>
2023-12-16 04:31:01 -0500

    Create Plugin.cs

---
+++ Hidden Abilities Mod/src/Plugin.cs
@@ -0,0 +1,40 @@
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HiddenAbilities.Patches;
+
+namespace HiddenAbilities
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class HiddenAbilitiesBase : BaseUnityPlugin
+    {
+        private const string ModGUID = "fawd.HiddenAbilities";
+        private const string ModName = "Hidden Abilities";
+        private const string ModVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(ModGUID);
+
+        private static HiddenAbilitiesBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(ModGUID);
+            mls.LogInfo("The hidden ability mod has awoken.");
+
+            harmony.PatchAll(typeof(HiddenAbilitiesBase));
+            harmony.PatchAll(typeof(HiddenSpawnPatch));
+            //harmony.PatchAll(typeof(HiddenPickupPatch));
+        }
+    }
+}
