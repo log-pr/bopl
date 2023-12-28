f59b91a5124067d10bf2fe5a4751d9fa00e103a0
Logan <110278043+logan-m-price@users.noreply.github.com>
2023-12-27 23:55:10 -0500

    add SlimControllerPatch.cs

---
+++ Hidden Abilities Mod/src/Patches/SlimeControllerPatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HiddenAbilitiesMod.Patches
+{
+    [HarmonyPatch(typeof(SlimeController))]
+    internal class SlimeControllerPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPrefix]
+        static void Patch(ref AbilityReadyIndicator[] ___AbilityReadyIndicators, ref NamedSpriteList ___abilityIconsFull)
+        {
+            if (___AbilityReadyIndicators != null)
+            {
+                foreach (AbilityReadyIndicator ari in ___AbilityReadyIndicators)
+                {
+                    ari.SetSprite(___abilityIconsFull.GetSprite(1), false);
+                }
+            }
+        }
+    }
+}
