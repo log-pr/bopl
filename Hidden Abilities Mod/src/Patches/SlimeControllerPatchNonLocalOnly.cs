d3b3e86cb4ea56f07dbd15e0dcfe3797b99d11ba
Logan <110278043+logan-m-price@users.noreply.github.com>
2024-01-17 04:49:29 -0500

    Add SlimeControllerPatch

---
+++ Hidden Abilities Mod/src/Patches/SlimeControllerPatchNonLocalOnly.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HiddenAbilitiesMod.Patches
+{
+    [HarmonyPatch(typeof(SlimeController))]
+    internal class SlimeControllerPatchNonLocalOnly
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPrefix]
+        static void Patch(ref AbilityReadyIndicator[] ___AbilityReadyIndicators, ref NamedSpriteList ___abilityIconsFull, ref int ___playerNumber)
+        {
+            //if abilityready indicators exists and the player is not a local player
+            if (___AbilityReadyIndicators != null && !(PlayerHandler.Get().GetPlayer(___playerNumber).IsLocalPlayer))
+            {
+                foreach (AbilityReadyIndicator ari in ___AbilityReadyIndicators)
+                {
+                    ari.SetSprite(___abilityIconsFull.GetSprite(1), false);
+                }
+            }
+        }
+    }
+}
